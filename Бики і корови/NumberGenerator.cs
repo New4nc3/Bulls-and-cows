bee4ae6b29d9b8963f93c3460817051ace31e724
new4nc3 <dzhurabayev@gmail.com>
2020-07-25 09:58:54 +0200

    Add project files.

---
+++ Бики і корови/NumberGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bulls_and_cows
+{
+    class NumberGenerator
+    {
+        public int DigitCount { get; } = 4;
+        public byte[] RandomedDigits { get; } = new byte[4];
+        public uint Randomed
+        {
+            get
+            {
+                if (randomed_ == 0)
+                {
+                    string temp = string.Empty;
+
+                    for (int i = 0; i < RandomedDigits.Length; i++)
+                        temp += RandomedDigits[i].ToString();
+
+                    randomed_ = Convert.ToUInt32(temp);
+                }
+
+                return randomed_;
+            }
+        }
+
+        private uint randomed_ = 0;
+
+        private List<byte> _availableDigits;
+        private Random _random;
+
+        public NumberGenerator(int digitCount = 4)
+        {
+            DigitCount = digitCount;
+            RandomedDigits = new byte[DigitCount];
+            _random = new Random();
+        }
+
+        public void GenerateNumberWithUniqueDigits()
+        {
+            randomed_ = 0;
+            _availableDigits = GetAvailableDigits();
+            RandomedDigits[0] = Convert.ToByte(GetAndExcludeRandomDigit(1));
+
+            for (int i = 1; i < DigitCount; i++)
+            {
+                RandomedDigits[i] = GetAndExcludeRandomDigit();
+            }
+        }
+
+        private static List<byte> GetAvailableDigits()
+        {
+            return new List<byte>()
+            {
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+            };
+        }
+
+        private byte GetAndExcludeRandomDigit(int minIndex = 0)
+        {
+            int index = _random.Next(minIndex, _availableDigits.Count);
+            byte toReturn = _availableDigits[index];
+
+            _availableDigits.RemoveAt(index);
+            return toReturn;
+        }
+    }
+}
---
+++ Бики і корови/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bulls_and_cows
+{
+    class Program
+    {
+        private static NumberGenerator _numGen;
+        private static List<byte> _guessedDigits;
+
+        static void Main()
+        {
+            string guessedString;
+            uint guessedNumber;
+            uint triesCount;
+
+            bool repeatGuessing;
+            bool playAgain;
+
+            Console.Title = "Bulls and cows";
+
+            do
+            {
+                _numGen = new NumberGenerator();
+                _numGen.GenerateNumberWithUniqueDigits();
+                triesCount = 0;
+                Console.Clear();
+                Console.WriteLine($"I've randomed new {_numGen.DigitCount}-digits number. Try to guess it!");
+
+                do
+                {
+                    repeatGuessing = true;
+                    guessedString = Console.ReadLine();
+
+                    if (guessedString.Length != _numGen.DigitCount || !uint.TryParse(guessedString, out guessedNumber)
+                        || guessedNumber.ToString().Length != _numGen.DigitCount)
+                    {
+                        Console.WriteLine($"Try to input {_numGen.DigitCount}-digits positive number");
+                        repeatGuessing = true;
+                        continue;
+                    }
+
+                    if (IsNumberValid(guessedNumber))
+                    {
+                        triesCount++;
+                        repeatGuessing = AnalyzeNumber(guessedNumber);
+                    }
+
+                } while (repeatGuessing);
+
+                if (triesCount == 1)
+                {
+                    Console.WriteLine($"\nAbsolutely amazing! You guess my randomed number {_numGen.Randomed} at first try! Unbelievable!");
+                }
+                else
+                {
+                    Console.WriteLine($"\nYou won! I've randomed {_numGen.Randomed} and you guessed it by {triesCount} tries.");
+                }
+
+                Console.WriteLine("\nDo you want to play again?\n <Enter> - Yes\n <Any key> - No");
+                playAgain = Console.ReadKey(true).Key == ConsoleKey.Enter ? true : false;
+
+            } while (playAgain);
+
+            Console.WriteLine("\n\nPowered by [new4nc3__#]\nPress any key to exit . . .");
+            Console.ReadKey(true);
+        }
+
+        private static bool IsNumberValid(uint number)
+        {
+            byte digit;
+            _guessedDigits = new List<byte>();
+
+            while (number > 0)
+            {
+                digit = (byte)(number % 10);
+                if (_guessedDigits.Contains(digit))
+                {
+                    Console.WriteLine("Try to input number without digit-repeating");
+                    return false;
+                }
+
+                _guessedDigits.Add(digit);
+                number /= 10;
+            }
+
+            _guessedDigits.Reverse();
+            return true;
+        }
+
+        private static bool AnalyzeNumber(uint number)
+        {
+            byte bulls = 0;
+            byte cows = 0;
+            string output = string.Empty;
+            string wordEnd;
+
+            for (int i = 0; i < _guessedDigits.Count; i++)
+            {
+                if (_numGen.RandomedDigits.Contains(_guessedDigits[i]))
+                {
+                    if (_numGen.RandomedDigits[i] == _guessedDigits[i])
+                        bulls++;
+                    else
+                        cows++;
+                }
+            }
+
+            if (bulls == 0 && cows == 0)
+            {
+                output = "No bulls or cows found";
+            }
+            else
+            {
+                if (bulls > 0)
+                {
+                    if (bulls == _guessedDigits.Count)
+                    {
+                        return false;
+                    }
+
+                    wordEnd = bulls > 1 ? "s" : string.Empty;
+                    output = $"Bull{wordEnd}: {bulls}. ";
+                }
+
+                if (cows > 0)
+                {
+                    wordEnd = cows > 1 ? "s" : string.Empty;
+                    output += $"Cow{wordEnd}: {cows}.";
+                }
+            }
+
+            Console.WriteLine(output);
+            return true;
+        }
+    }
+}
